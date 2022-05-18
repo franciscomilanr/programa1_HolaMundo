d90a25935e9277e44c2711aa0929f29786483040
franciscomilanr <105766750+franciscomilanr@users.noreply.github.com>
2022-05-18 15:36:57 -0500

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,29 @@
+
+/* Autor: Franciscomilan
+	Escuela: Universidad uvm
+	Profesor: Armando Cruz
+	Materia: Programacion Estrucutrada
+	Ciclo: 01/2022
+	Realizado: 03/02/2022
+	Descripcion: Primer programa en C llamado Hola mundo el cual muestra la estructura basica para 	la materia de programacion estructurada.
+*/
+
+
+/* Significados
+\n : Es para dar un salto de linea
+#include : Incluir las directivas del procesador
+<stdio.h> : Libreria que contiene las funciones de entrada y salida 
+return 0 : Indicarle al programa que termino exitosamente
+*/
+
+//Libreria
+#include<stdio.h>
+
+//Principal
+int main() {
+	printf("Hola mundo !!!\n\n");
+	printf("franciscomilan\n");
+
+	//Retorno
+	return 0;
+}
